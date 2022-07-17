a9a26cbe06b2b6a957ee7dabe38723c7544b7481
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 11:49:03 -0400

    add action for saving user's answer

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,18 @@
 export const GET_USERS = "GET_USERS";
+export const ADD_USER_ANSWER = "ADD_USER_ANSWER";
 
 export function getUsers(users) {
   return {
     type: GET_USERS,
     users,
   };
+}
+
+export function addUserAnswer(authedUser, qid, answer) {
+    return {
+        type: ADD_USER_ANSWER,
+        authedUser,
+        qid,
+        answer,
+    };
 }
