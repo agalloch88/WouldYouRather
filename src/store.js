be1ef3d89b1eac140ff010289c74ad01ec280f22
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 10:29:57 -0400

    update store with reducers

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import authedUser from "./reducers/authedUser";
+import polls from "./reducers/polls";
+import users from "./reducers/users";
 
 export const store = configureStore({
   reducer: {
+    authedUser,
+    polls,
+    users,
   },
 });
