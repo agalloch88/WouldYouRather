45bc268a82610ec0dbaa9759a4a54cba321df952
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 16:10:55 -0400

    add setAuthedUser to handleInitialData

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,12 @@
 import { getInitialData } from "../util/api";
 import { getUsers } from "./users";
 import { getQuestions } from "./polls";
+import { setAuthedUser } from "./authedUser";
 
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData().then(({ users, questions }) => {
+      dispatch(setAuthedUser(JSON.parse(sessionStorage.getItem("authedUser"))));
       dispatch(getUsers(users));
       dispatch(getQuestions(questions));
     });
