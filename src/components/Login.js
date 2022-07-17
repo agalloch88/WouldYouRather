ff4986088ef486fb3297fa6c7adba16d32f78c3d
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 10:26:19 -0400

    create Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,49 @@
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleLogin } from "../actions/authedUser";
 
-const Login = (props) => {
+const Login = ({ dispatch }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleUsername = (e) => {
+    const value = e.target.value;
+    setUsername(value);
+  };
+
+  const handlePassword = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(handleLogin(username, password));
+    navigate("/");
+  };
+
   return (
     <div>
-      <h1>Login here</h1>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsername}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          value={password}
+          onChange={handlePassword}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
