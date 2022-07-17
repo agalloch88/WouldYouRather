6fa4b26dd7ccd72bfb94aaa6190936c62cd02734
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 16:07:26 -0400

    create logout user actions

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,19 +1,37 @@
 export const SET_AUTHED_USER = "SET_AUTHED_USER";
+export const LOGOUT_AUTHED_USER = "LOGOUT_AUTHED_USER";
 
-export function setAuthedUser(id) {
+export function setAuthedUser(authedUser) {
   return {
     type: SET_AUTHED_USER,
-    id,
+    authedUser,
   };
 }
+
+export function logoutAuthedUser() {
+  return {
+    type: LOGOUT_AUTHED_USER,
+  };
+}
+
 export function handleLogin(username, password) {
   return (dispatch, getState) => {
     const { users } = getState();
 
-    const user = Object.values(users).find((user) => user.id === username && user.password === password);
+    const user = Object.values(users).find(
+      (user) => user.id === username && user.password === password
+    );
 
     if (user) {
-        return dispatch(setAuthedUser(user));
+      sessionStorage.setItem("authedUser", JSON.stringify(user));
+      return dispatch(setAuthedUser(user));
     }
+  };
+}
+
+export function handleLogout() {
+  return (dispatch) => {
+    sessionStorage.removeItem("authedUser");
+    return dispatch(logoutAuthedUser());
   };
 }
