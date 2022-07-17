47f16b439e14d4e9561533586bb8a2bc3d9aeca5
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 11:56:50 -0400

    add new actions to polls for user/answers

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,12 +1,23 @@
-import { saveQuestion } from "../util/api";
+import { saveQuestion, saveQuestionAnswer } from "../util/api";
+import { addUserAnswer } from "./users";
 
 export const ADD_QUESTION = "ADD_QUESTION";
+export const ADD_QUESTION_ANSWER = "ADD_QUESTION_ANSWER";
 export const GET_QUESTIONS = "GET_QUESTIONS";
 
 function addQuestion(question) {
   return {
     type: ADD_QUESTION,
     question,
+  };
+}
+
+function addQuestionAnswer(author, qid, answer) {
+  return {
+    type: ADD_QUESTION_ANSWER,
+    author,
+    qid,
+    answer,
   };
 }
 
@@ -24,5 +35,15 @@ export function getQuestions(questions) {
   return {
     type: GET_QUESTIONS,
     questions,
+  };
+}
+
+export function handleAddQuestionAnswer(qid, answer) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    return saveQuestionAnswer(authedUser.id, qid, answer).then(() => {
+      dispatch(addQuestionAnswer(authedUser.id, qid, answer));
+      dispatch(addUserAnswer(authedUser.id, qid, answer));
+    });
   };
 }
