e4d05ea2a3b6f6e7ac6c2b190779888573685727
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 10:02:10 -0400

    set up initial data load in App.js, display of Login or Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nav from "./components/Nav";
 import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 import NewPoll from "./components/NewPoll";
 import PollPage from "./components/PollPage";
 import { Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
+import { handleInitialData } from "./actions/shared";
 import "./App.css";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+
   return (
     <div className="app__container">
       <Nav />
-      <Routes>
-        <Route path="/" exact element={<Dashboard />} />
-        <Route path="/questions/:id" element={<PollPage />} />
-        <Route path="/new" element={<NewPoll />} />
-      </Routes>
+      {false ? (
+        <Login />
+      ) : (
+        <Routes>
+          <Route path="/" exact element={<Dashboard />} />
+          <Route path="/questions/:id" element={<PollPage />} />
+          <Route path="/new" element={<NewPoll />} />
+        </Routes>
+      )}
     </div>
   );
 }
 
-const mapStateToProps = ({}) => ({});
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
 
 export default connect(mapStateToProps)(App);
