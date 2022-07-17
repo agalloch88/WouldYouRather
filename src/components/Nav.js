973a3f65f46d1af57cbafb9ff443ca314d68daf7
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 09:14:00 -0400

    start building Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,24 @@
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Nav = (props) => {
   return (
-    <ul>
-      <li>Dashboard here</li>
-      <li>.</li>
-      <li>.</li>
-    </ul>
+    <nav className="nav__container">
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/leaderboard">Leaderboard</Link>
+            </li>
+            <li>
+                <Link to="/new">New Poll</Link>
+            </li>
+            <li>
+                <Link to="/">Sign In/Sign Out</Link>
+            </li>
+        </ul>
+    </nav>
   );
 };
 
