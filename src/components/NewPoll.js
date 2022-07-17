b5c5b423abe3e2da874ab765cdefca41306c8e38
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 09:33:50 -0400

    build NewPoll form

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const NewPoll = (props) => {
+const NewPoll = ({ dispatch }) => {
+  const [firstChoice, setFirstChoice] = useState("");
+  const [secondChoice, setSecondChoice] = useState("");
+  const navigate = useNavigate();
+
+  const handleFirstChoiceChange = (e) => {
+    const value = e.target.value;
+    setFirstChoice(value);
+  };
+
+  const handleSecondChoiceChange = (e) => {
+    const value = e.target.value;
+    setSecondChoice(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: dispatch action to create new poll
+    navigate("/");
+  };
   return (
     <div>
-      <h1>New Poll here</h1>
+      <h1>New Poll</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstChoice">First Choice</label>
+        <input
+          type="text"
+          id="firstChoice"
+          value={firstChoice}
+          onChange={handleFirstChoiceChange}
+        />
+        <label htmlFor="secondChoice">Second Choice</label>
+        <input
+          type="text"
+          id="secondChoice"
+          value={secondChoice}
+          onChange={handleSecondChoiceChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
