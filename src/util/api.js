2cdcf046a30c4ca29fe6302f26fc7418b800fceb
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 09:49:07 -0400

    set up getInitialData function

--- src/util/api.js
+++ src/util/api.js
@@ -1,5 +1,11 @@
-import { _saveQuestion } from "./_DATA";
+import { _getQuestions, _saveQuestion, _getUsers } from "./_DATA";
+
+export function getInitialData() {
+  return Promise.all([_getUsers(), _getQuestions()]).then(
+    ([users, questions]) => ({ users, questions })
+  );
+}
 
 export function saveQuestion(choiceOneText, choiceTwoText, author) {
-    return _saveQuestion({choiceOneText, choiceTwoText, author});
+  return _saveQuestion({ choiceOneText, choiceTwoText, author });
 }
