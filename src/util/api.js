18ce5aa2741a3c34c4cb3b5063efb542a94ca55a
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 11:46:42 -0400

    add _saveQuestionAnswer to APi

--- src/util/api.js
+++ src/util/api.js
@@ -1,4 +1,9 @@
-import { _getQuestions, _saveQuestion, _getUsers } from "./_DATA";
+import {
+  _getQuestions,
+  _saveQuestion,
+  _getUsers,
+  _saveQuestionAnswer,
+} from "./_DATA";
 
 export function getInitialData() {
   return Promise.all([_getUsers(), _getQuestions()]).then(
@@ -8,4 +13,12 @@ export function getInitialData() {
 
 export function saveQuestion(choiceOneText, choiceTwoText, author) {
   return _saveQuestion({ choiceOneText, choiceTwoText, author });
+}
+
+export function saveQuestionAnswer(authedUserId, qid, answer) {
+  return _saveQuestionAnswer({
+    authedUser: authedUserId,
+    qid,
+    answer,
+  });
 }
