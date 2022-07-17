2fa39350937e3b54fa054568141545c1d91835dc
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 09:10:12 -0400

    import components, create routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav";
+import Dashboard from "./components/Dashboard";
+import NewPoll from "./components/NewPoll";
+import PollPage from "./components/PollPage";
+import { Routes, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Test
+    <div className="app__container">
+      <Nav />
+      <Routes>
+        <Route path="/" exact element={<Dashboard />} />
+        <Route path="/questions/:id" element={<PollPage />} />
+        <Route path="/new" element={<NewPoll />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = ({}) => ({});
+
+export default connect(mapStateToProps)(App);
