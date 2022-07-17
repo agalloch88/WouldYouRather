9391a90ed0e4542b56abc41a18d846852518769f
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 16:27:57 -0400

    refactor Dashboard, filter for unanswered vs answered questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,38 +1,43 @@
 import { connect } from "react-redux";
 import PollCard from "./PollCard";
 
-const Dashboard = (props) => {
+const Dashboard = ({ authedUser, questions }) => {
+  const unanswered = (question) =>
+    !question.optionOne.votes.includes(authedUser.id) &&
+    !question.optionTwo.votes.includes(authedUser.id);
+
+  const answered = (question) =>
+    question.optionOne.votes.includes(authedUser.id) ||
+    question.optionTwo.votes.includes(authedUser.id);
+
   return (
     <div>
       <h1>Dashboard</h1>
       <h2>New Questions</h2>
       <ul>
-        {Object.values(props.questions)
-          .filter((question) => true)
-          .map((question) => (
-            <li key={question.id}>
-                {question.id}
-                <PollCard question={question} />
-            </li>
-          ))}
+        {questions.filter(unanswered).map((question) => (
+          <li key={question.id}>
+            {question.id}
+            <PollCard question={question} />
+          </li>
+        ))}
       </ul>
       <h2>Completed Questions</h2>
       <ul>
-        {Object.values(props.questions)
-          .filter((question) => true)
-          .map((question) => (
-            <li key={question.id}>
-                {question.id}
-                <PollCard question={question} />
-            </li>
-          ))}
+        {questions.filter(answered).map((question) => (
+          <li key={question.id}>
+            {question.id}
+            <PollCard question={question} />
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
 
-const mapStateToProps = ({ questions }) => ({
-  questions,
+const mapStateToProps = ({ authedUser, questions }) => ({
+  authedUser,
+  questions: Object.values(questions).sort((a, b) => b.timestamp - a.timestamp),
 });
 
 export default connect(mapStateToProps)(Dashboard);
