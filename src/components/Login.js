a3fb27737c0d48195cad0095d0eb3e584d54c254
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 17:28:09 -0400

    style Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,28 +26,53 @@ const Login = ({ dispatch }) => {
 
   return (
     <div>
-      <h1>Login</h1>
+      <h1 className="text-3xl font-bold mt-9">Login</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={handleUsername}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="text"
-          id="password"
-          value={password}
-          onChange={handlePassword}
-        />
-        <button type="submit">Submit</button>
+        <div>
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-slate-700"
+          >
+            Username
+          </label>
+          <div className="mt-1">
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsername}
+              className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
+            />
+          </div>
+        </div>
+        <div className="mt-6">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-slate-700"
+          >
+            Password
+          </label>
+          <div className="mt-1">
+            <input
+              type="text"
+              id="password"
+              value={password}
+              onChange={handlePassword}
+              className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
+            />
+          </div>
+        </div>
+        <div className="mt-6 text-right">
+          <button
+            type="submit"
+            className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white"
+          >
+            Sign In
+          </button>
+        </div>
       </form>
     </div>
   );
 };
 
-const mapStateToProps = ({}) => ({});
-
-export default connect(mapStateToProps)(Login);
+export default connect()(Login);
