70b060e9099c67090830bebec79a1daeaf957cb8
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 10:45:21 -0400

    import polls action to polls reducer, set up case

--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,5 +1,12 @@
+import { GET_QUESTIONS } from "../actions/polls";
+
 export default function polls(state = {}, action) {
   switch (action.type) {
+    case GET_QUESTIONS:
+        return {
+            ...state,
+            ...action.questions,
+        };
     default:
       return state;
   }
