774727582eab254c4c454115091b59413580e3d8
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 12:02:21 -0400

    add cases for ADD_QUESTION, ADD_QUESTION_ANSWER to polls reducer

--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,12 +1,34 @@
-import { GET_QUESTIONS } from "../actions/polls";
+import {
+  GET_QUESTIONS,
+  ADD_QUESTION,
+  ADD_QUESTION_ANSWER,
+} from "../actions/polls";
 
 export default function polls(state = {}, action) {
   switch (action.type) {
     case GET_QUESTIONS:
-        return {
-            ...state,
-            ...action.questions,
-        };
+      return {
+        ...state,
+        ...action.questions,
+      };
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.id]: action.question,
+      };
+    case ADD_QUESTION_ANSWER:
+      return {
+        ...state,
+        [action.qid]: {
+          ...state[action.qid],
+          [action.answer]: {
+            ...state[action.qid][action.answer],
+            votes: state[action.qid][action.answer].votes.concat([
+              action.authedUser,
+            ]),
+          },
+        },
+      };
     default:
       return state;
   }
