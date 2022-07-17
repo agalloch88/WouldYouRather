c72ca8fb5fb50397a8bc8635eadfa9976e65f3e3
Ryan Kirsch <agalloch88@gmail.com>
2022-07-17 11:13:40 -0400

    build PollPage

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,12 +1,36 @@
 import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 
-const PollPage = (props) => {
+const PollPage = ({ users, questions }) => {
+  const { id } = useParams();
+  const { question, user } = initRequiredData(id, questions, users);
+
   return (
     <div>
-      <h1>Poll</h1>
+      <h1>Poll by {user.id}</h1>
+      <img src={user.avatarURL} alt={`Profile of ${user.name}`} className="avatar" />
+
+        <h2>Would you rather...</h2>
+
+        <p>{question.firstChoice.text}</p>
+        <button>Click</button>
+
+        <p>{question.secondChoice.text}</p>
+        <button>Click</button>
     </div>
   );
 };
+
+const initRequiredData = (users, questions, questionId) => {
+    try {
+        const question = Object.values(questions).find((question) => question.id === questionId);
+        const author = question.author;
+        const user = Object.values(users).find((user) => user.id === author);
+        return { question, user };
+    } catch (error) {
+        throw new Error(`Question or user not found. ${error}`);
+    }
+}
 
 const mapStateToProps = ({}) => ({});
 
