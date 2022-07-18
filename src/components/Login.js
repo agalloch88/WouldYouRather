fa9e9e46446bb6d4059177618ceb0c1b4d609fa1
Ryan Kirsch <agalloch88@gmail.com>
2022-07-18 07:06:13 -0400

    test for Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -39,6 +39,7 @@ const Login = ({ dispatch }) => {
             <input
               type="text"
               id="username"
+              data-testid="username"
               value={username}
               onChange={handleUsername}
               className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
@@ -56,6 +57,7 @@ const Login = ({ dispatch }) => {
             <input
               type="text"
               id="password"
+              data-testid="password"
               value={password}
               onChange={handlePassword}
               className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
@@ -65,6 +67,7 @@ const Login = ({ dispatch }) => {
         <div className="mt-6 text-right">
           <button
             type="submit"
+            data-testid="submit"
             className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white"
           >
             Sign In
